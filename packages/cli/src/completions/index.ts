186f5318010eae8353297d41f50eed8dedb3e992
Mohamed Hedi CHLAGOU <chlagoumedhedi@outlook.com>
2026-03-11 17:50:32 +0100

    feat(cli): add completions barrel export

---
+++ packages/cli/src/completions/index.ts
@@ -0,0 +1,4 @@
+export { BASH_COMPLETION_SCRIPT } from "./bash";
+export { ZSH_COMPLETION_SCRIPT } from "./zsh";
+export { FISH_COMPLETION_SCRIPT } from "./fish";
+export { handleGetCompletions } from "./get-completions";
